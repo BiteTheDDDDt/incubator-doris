ad21cd95bfac82e7e9a9b97897440832b617a464
BiteTheDDDDt <952130278@qq.com>
2021-11-05 11:06:46 +0800

    refactor predicate function creator

--- be/src/exprs/in_predicate.h
+++ be/src/exprs/in_predicate.h
@@ -22,6 +22,7 @@
 #include <string>
 #include <unordered_set>
 
+#include "exprs/create_predicate_function.h"
 #include "exprs/hybrid_set.h"
 #include "exprs/predicate.h"
 #include "runtime/raw_value.h"
