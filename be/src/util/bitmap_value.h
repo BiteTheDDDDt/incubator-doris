42af4716a7079fcd635f72fa573994536a80588e
Jerry Hu <mrhhsg@gmail.com>
2024-06-28 11:29:03 +0800

    [fix](bitmap) incorrect type of BitmapValue with fastunion (#36834) (#36896)

--- be/src/util/bitmap_value.h
+++ be/src/util/bitmap_value.h
@@ -1306,7 +1306,6 @@ public:
             case SINGLE: {
                 _set.insert(_sv);
                 _type = SET;
-                _convert_to_bitmap_if_need();
                 break;
             }
             case BITMAP:
@@ -1317,9 +1316,11 @@ public:
                 _type = BITMAP;
                 break;
             case SET: {
-                _convert_to_bitmap_if_need();
                 break;
             }
+            }
+            if (_type == SET) {
+                _convert_to_bitmap_if_need();
             }
         }
 
