685c97af096ced7e390fe9323c184ace56d7c6e1
BiteTheDDDDt <pxl290@qq.com>
2024-10-24 18:12:09 +0800

    update

--- be/src/vec/common/hash_table/hash_key_type.h
+++ be/src/vec/common/hash_table/hash_key_type.h
@@ -97,12 +97,13 @@ inline HashKeyType get_hash_key_type(const std::vector<vectorized::DataTypePtr>&
         return HashKeyType::without_key;
     }
 
+    auto t = remove_nullable(data_types[0]);
     // serialized cannot be used in the case of single column, because the join operator will have some processing of column nullable, resulting in incorrect serialized results.
-    if (!data_types[0]->have_maximum_size_of_value() && is_string(remove_nullable(data_types[0]))) {
+    if (!t->have_maximum_size_of_value() && is_string(t)) {
         return HashKeyType::string_key;
     }
 
-    size_t size = remove_nullable(data_types[0])->get_maximum_size_of_value_in_memory();
+    size_t size = t->get_maximum_size_of_value_in_memory();
     if (size == sizeof(vectorized::UInt8)) {
         return HashKeyType::int8_key;
     } else if (size == sizeof(vectorized::UInt16)) {
@@ -117,7 +118,7 @@ inline HashKeyType get_hash_key_type(const std::vector<vectorized::DataTypePtr>&
         return HashKeyType::int256_key;
     } else {
         throw Exception(ErrorCode::INTERNAL_ERROR, "meet invalid type size, size={}, type={}", size,
-                        data_types[0]->get_name());
+                        t->get_name());
     }
 }
 
