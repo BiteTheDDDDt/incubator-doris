fe2a4bf1c4ea08c05cf1d8d1c833ecbe671810f6
BiteTheDDDDt <xl@selectdb.com>
2026-03-05 16:34:33 +0800

    adjust single streaming threshold

--- be/src/runtime/fragment_mgr.cpp
+++ be/src/runtime/fragment_mgr.cpp
@@ -922,6 +922,7 @@ Status FragmentMgr::exec_plan_fragment(const TPipelineFragmentParams& params,
 
     if (!params.__isset.need_wait_execution_trigger || !params.need_wait_execution_trigger) {
         query_ctx->set_ready_to_execute_only();
+        query_ctx->set_single_backend_query(true);
     }
 
     query_ctx->set_pipeline_context(params.fragment_id, context);
